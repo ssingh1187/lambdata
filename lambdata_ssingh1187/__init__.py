1daa1a7988cf425a89a0afb3a7bfa718084de5f7
ssingh1187 <sshilpa1187@users.noreply.github.com>
2019-03-21 01:46:32 -0700

    Added two helper functions.

--- lambdata_ssingh1187/__init__.py
+++ lambdata_ssingh1187/__init__.py
@@ -5,5 +5,20 @@ lambdata - A collection of Data Science helper functions.
 import pandas as pd
 import numpy as np
 
-ZEROS = pd.DataFrame(np.zeros(50))
-ONES = pd.DataFrame(np.ones(10))
+
+"""
+Convert all missing value in a specified column to the median.
+"""
+def missing_median(df, name):
+  median = df[name].median()
+  df[name] = df[name].fillna(median)
+
+
+"""
+Check a dataframe for nulls.
+"""
+def nulls_in_a_df(df, name):
+  if df[name].isna().sum()>=0:
+    print('DataFrame has null values')
+  else:
+    print('There are no nulls')
